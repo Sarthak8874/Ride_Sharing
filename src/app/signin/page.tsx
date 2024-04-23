0acd9a773429e68b37047567c57e6d69ace02b34
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-04-23 23:44:47 +0530

    signin component added

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SigninForm from '@/components/SigninForm'
+
+
+function page() {
+  return (
+    <div>
+        <SigninForm/>
+    </div>
+  )
+}
+
+export default page
--- src/components/SigninForm.tsx
+++ src/components/SigninForm.tsx
@@ -5,33 +5,22 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/inputA";
 import { cn } from "@/utils/cn";
 
-export function SignupForm() {
+export function SigninForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
   };
   return (
-    <div className="sm:h-auto   w-full md:h-[100vh] flex justify-center items-center ">
+    <div className="w-[350px] md:w-full mx-auto lg:w-full h-[100vh] flex justify-center items-center ">
       <div className="max-w-md  w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
         <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-          Welcome to Ride Sharing
+        Welcome back 
         </h2>
         <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-          Welcome aboard our blockchain-powered ridesharing platform, where
-          every journey is secure, transparent, and tailored just for you!
+          Please Sign in to continue
         </p>
 
         <form className="my-8" onSubmit={handleSubmit}>
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-            <LabelInputContainer>
-              <Label htmlFor="firstname">First name</Label>
-              <Input id="firstname" placeholder="Tyler" type="text" />
-            </LabelInputContainer>
-            <LabelInputContainer>
-              <Label htmlFor="lastname">Last name</Label>
-              <Input id="lastname" placeholder="Durden" type="text" />
-            </LabelInputContainer>
-          </div>
           <LabelInputContainer className="mb-4">
             <Label htmlFor="email">Email Address</Label>
             <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
@@ -40,20 +29,13 @@ export function SignupForm() {
             <Label htmlFor="password">Password</Label>
             <Input id="password" placeholder="••••••••" type="password" />
           </LabelInputContainer>
-          <LabelInputContainer className="mb-8">
-            <Label htmlFor="twitterpassword">Confirm password</Label>
-            <Input
-              id="twitterpassword"
-              placeholder="••••••••"
-              type="twitterpassword"
-            />
-          </LabelInputContainer>
+          
 
           <button
             className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
             type="submit"
           >
-            Sign up &rarr;
+            Sign in &rarr;
           </button>
 
           <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
@@ -62,7 +44,6 @@ export function SignupForm() {
     </div>
   );
 }
-
 
 const LabelInputContainer = ({
   children,
@@ -78,4 +59,4 @@ const LabelInputContainer = ({
   );
 };
 
-export default SignupForm;
+export default SigninForm;
