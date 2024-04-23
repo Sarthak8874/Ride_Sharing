1d01e1b2ddebfb4a927197980d419d7e68b1bb7d
Harsh Agarwal <harshulkh@gmail.com>
2024-04-23 23:38:34 +0530

    update

--- src/components/RegisterVehicle.tsx
+++ src/components/RegisterVehicle.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import {Label} from "@/components/ui/label";
 // import { Label } from "../ui/label";
-import { Input } from "@/components/ui/input";
+import { Input } from "@/components/ui/inputA";
 import { cn } from "@/utils/cn";
 import {
   IconBrandGithub,
