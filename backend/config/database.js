95f8838545113fa63b6ecdc35452bbe78c82d71e
Pratham Choudhary <prathamchoudhary924@gmail.com>
2024-04-29 11:49:59 +0530

    backend added

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+const dbConnect = () => {
+    mongoose.connect(process.env.MONDODB_URL)
+    .then(()=>{console.log("DB connection is success")})
+    .catch((err)=>{
+        console.log("error in DB connection",err);
+    })
+}
+
+module.exports = dbConnect;
+
+
---
+++ backend/models/Published.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+
+const publishedSchema = new mongoose.Schema({
+    sourceId: {
+        type: String,
+        required: true
+    },
+    destinationId: {
+        type: String,
+        required: true
+    },
+    vehicleId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Vehicle',
+        required: true
+    },
+    driverId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    etherCost: {
+        type: Number,
+        required: true
+    },
+    distance: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: String,
+        required: true
+    },
+    endTime: {
+        type: String,
+        required: true
+    },
+    numberOfSeats: {
+        type: Number,
+        required: true
+    },
+    numberOfBookedSeats: {
+        type: Number,
+        default: 0 
+    },
+    rideBooked: {
+        type: Boolean,
+        default: false 
+    },
+    transactionIds: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Transactions'
+    }]
+});
+
+module.exports = mongoose.model("Published", publishedSchema);
