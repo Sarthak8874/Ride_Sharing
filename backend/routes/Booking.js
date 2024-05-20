f3ac63cdce009369140b4dbed0218a3a108a3da3
= <=>
2024-05-20 16:06:48 +0530

    distance radius logic added

--- backend/routes/Booking.js
+++ backend/routes/Booking.js
@@ -8,10 +8,9 @@ const Transactions = require("../models/Transactions");
 
 // Publish a new ride
 router.post("/publish", auth, async (req, res) => {
-
   const data = new Published({
     ...req.body,
-    driverId: req.user._id
+    driverId: req.user._id,
   });
   try {
     await data.save();
@@ -27,58 +26,93 @@ router.post("/publish", auth, async (req, res) => {
   }
 });
 
+const getDistance = async (sourceId, destinationId) => {
+  const apiKey = "AIzaSyDx5GRB6r2aS6ICayTDxpIX4wO71c4FniY"; // Replace with your actual API key
+  const url = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=place_id:${sourceId}&destinations=place_id:${destinationId}&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    const distance = response.data.rows[0].elements[0].distance.value; // distance in meters
+    // console.log(distance);
+    return distance; // convert to kilometers
+  } catch (error) {
+    // console.error('Error fetching distance from Google API:', error);
+    return null;
+  }
+};
+
 // Get feasible rides as per the search criteria
 router.get("/search", auth, async (req, res) => {
   try {
     const { sourceId, destinationId, date, seatsRequired } = req.query;
-    // Convert the seatsRequired to a number
- 
     const seats = parseInt(seatsRequired);
     const userId = req.user._id;
+
     if (seats <= 0 || isNaN(seats)) {
       return res.status(400).json({
         success: false,
         message: "Invalid number of seats",
       });
     }
+
     if (sourceId === destinationId) {
       return res.status(400).json({
         success: false,
-        message: "Source and destination cannot be same",
+        message: "Source and destination cannot be the same",
       });
     }
 
     // Find all feasible rides based on the provided criteria
-    const feasibleRides = await Published.find({
-      sourceId,
-      destinationId,
+    let feasibleRides = await Published.find({
       date: date,
       numberOfAvailableSeats: { $gte: seats },
       rideBooked: false,
-      // driverId: { $ne: userId },
-    }).populate('driverId').populate('vehicleId');
+    })
+      .populate("driverId")
+      .populate("vehicleId");
 
-    // Send the array of feasible rides as the response
-    const feasibleRidesWithoutIds = feasibleRides.map(ride => {
-      const { driverId, vehicleId, ...rideWithoutIds } = ride.toObject(); // Extract driverId and vehicleId
-      const driverDetails = {
-        firstName: ride.driverId.firstName,
-        lastName: ride.driverId.lastName,
-      };
-      const vehicleDetails = {
-        // vehicleNumber: ride.vehicleId.vehicleNumber,
-      };
-      return { ...rideWithoutIds, driver: driverDetails, vehicle: vehicleDetails };
-    });
-    
-    // Send the array of feasible rides with driver details as the response
+    // Filter rides based on matching destinationId
+    feasibleRides = feasibleRides.filter(
+      (ride) => ride.destinationId === destinationId
+    );
+
+    // Filter rides based on distance between sourceId and destinationId
+    const filteredRidesWithDistance = [];
+    for (const ride of feasibleRides) {
+      const distance = await getDistance(sourceId, ride.sourceId);
+      if (distance !== null && distance < 25) {
+        filteredRidesWithDistance.push({ ride, distance });
+      }
+    }
+
+    // Sort rides by distance
+    filteredRidesWithDistance.sort((a, b) => a.distance - b.distance);
+
+    // Map and prepare the response data
+    const feasibleRidesWithoutIds = filteredRidesWithDistance.map(
+      ({ ride }) => {
+        const { driverId, vehicleId, ...rideWithoutIds } = ride.toObject();
+        const driverDetails = {
+          firstName: ride.driverId.firstName,
+          lastName: ride.driverId.lastName,
+        };
+        const vehicleDetails = {
+          // Add any vehicle details if needed
+        };
+        return {
+          ...rideWithoutIds,
+          driver: driverDetails,
+          vehicle: vehicleDetails,
+        };
+      }
+    );
+
     res.status(200).json({
       success: true,
-      message: 'Feasible rides found',
+      message: "Feasible rides found",
       data: feasibleRidesWithoutIds,
     });
   } catch (error) {
-    // If there's an error, send an error response
     res.status(500).json({
       success: false,
       message: error.message,
@@ -105,13 +139,13 @@ router.post("/book/:id", auth, async (req, res) => {
         message: "Not enough available seats",
       });
     }
-    if(seatsRequired <= 0){
+    if (seatsRequired <= 0) {
       return res.status(400).json({
         success: false,
         message: "Invalid number of seats",
       });
     }
-    if(riderId.equals(ride.driverId)){
+    if (riderId.equals(ride.driverId)) {
       return res.status(400).json({
         success: false,
         message: "Driver cannot book his own ride",
@@ -121,15 +155,20 @@ router.post("/book/:id", auth, async (req, res) => {
     if (ride.numberOfAvailableSeats === 0) {
       ride.rideBooked = true;
     }
-    ride.riders.push(riderId)
-    const transaction = new Transactions({rideId, riderId, numberOfPassenger:seatsRequired, etherCost:ride.etherCost})
+    ride.riders.push(riderId);
+    const transaction = new Transactions({
+      rideId,
+      riderId,
+      numberOfPassenger: seatsRequired,
+      etherCost: ride.etherCost,
+    });
     await transaction.save();
     ride.transactionIds.push(transaction);
     await ride.save();
     res.status(200).json({
       success: true,
       message: "Ride booked successfully",
-      data:ride,
+      data: ride,
     });
   } catch (error) {
     res.status(500).json({
@@ -143,7 +182,9 @@ router.post("/book/:id", auth, async (req, res) => {
 router.get("/book/:id", auth, async (req, res) => {
   try {
     const rideId = req.params.id;
-    const ride = await Published.findById(rideId).populate('driverId').populate('vehicleId');
+    const ride = await Published.findById(rideId)
+      .populate("driverId")
+      .populate("vehicleId");
     if (ride === null) {
       return res.status(404).json({
         success: false,
@@ -160,14 +201,17 @@ router.get("/book/:id", auth, async (req, res) => {
     const vehicleDetails = {
       // vehicleNumber: ride.vehicleId.vehicleNumber,
     };
-    const data = { ...rideWithoutIds, driver: driverDetails, vehicle: vehicleDetails };
+    const data = {
+      ...rideWithoutIds,
+      driver: driverDetails,
+      vehicle: vehicleDetails,
+    };
 
     res.status(200).json({
       success: true,
       message: "Ride found",
       data,
     });
-    
   } catch (error) {
     res.status(500).json({
       success: false,
